70e7bdba0559de954013d933833277a7e05c8e66
Shiv Raj Badu <shivrajbadu@gmail.com>
2023-12-25 01:30:46 +0545

    latest build

--- assets/js/data/swcache.js
+++ assets/js/data/swcache.js
@@ -23,19 +23,19 @@ const resource = [
   /* --- Favicons & compressed JS --- */
   
   
+    '/assets/js/dist/categories.min.js',
+    '/assets/js/dist/commons.min.js',
+    '/assets/js/dist/home.min.js',
+    '/assets/js/dist/misc.min.js',
+    '/assets/js/dist/page.min.js',
+    '/assets/js/dist/post.min.js',
     '/assets/img/favicons/android-chrome-192x192.png',
     '/assets/img/favicons/android-chrome-512x512.png',
     '/assets/img/favicons/apple-touch-icon.png',
     '/assets/img/favicons/favicon-16x16.png',
     '/assets/img/favicons/favicon-32x32.png',
     '/assets/img/favicons/favicon.ico',
-    '/assets/img/favicons/mstile-150x150.png',
-    '/assets/js/dist/categories.min.js',
-    '/assets/js/dist/commons.min.js',
-    '/assets/js/dist/home.min.js',
-    '/assets/js/dist/misc.min.js',
-    '/assets/js/dist/page.min.js',
-    '/assets/js/dist/post.min.js'
+    '/assets/img/favicons/mstile-150x150.png'
 ];
 
 /* The request url with below domain will be cached */
@@ -45,7 +45,7 @@ const allowedDomains = [
     'www.google-analytics.com',
   
 
-  'shivrajbadu.com.np',
+  'localhost:4000',
 
   
 
--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1703438637';
+const cacheName = 'chirpy-1703446974';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
