a31c114e43dd72446578dae6f9164008171c52be
Shiv Raj Badu <shivrajbadu@gmail.com>
2024-01-03 00:34:15 +0545

    add post on automl

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1704197761';
+const cacheName = 'chirpy-1704221225';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
