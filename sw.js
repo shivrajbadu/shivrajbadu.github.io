445fbd80474cde8b0edf030ef9567bd3e6fcfd0d
Shiv Raj Badu <shivrajbadu@gmail.com>
2024-01-06 11:38:36 +0545

    updated g-tag

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1704221225';
+const cacheName = 'chirpy-1704520305';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
