de36e1e747401e132498c52c6c89f4d3fd0c8bf6
Shiv Raj Badu <shivrajbadu@gmail.com>
2024-02-09 20:47:19 +0545

    tailwind-css notes added

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1707473878';
+const cacheName = 'chirpy-1707490865';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
