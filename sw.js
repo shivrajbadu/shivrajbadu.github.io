61732061fc881ef47e766db6f22b019750eda475
Shiv Raj Badu <shivrajbadu@gmail.com>
2024-02-11 19:27:46 +0545

    tailwind button syling article

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1707653086';
+const cacheName = 'chirpy-1707658900';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
