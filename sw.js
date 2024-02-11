44bea5db60e8ce484de87791cdd3c24f2620ea5f
Shiv Raj Badu <shivrajbadu@gmail.com>
2024-02-11 21:36:57 +0545

    tailwind form components

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1707658900';
+const cacheName = 'chirpy-1707666651';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
