568476c44b43d0c31e46f6429acabc04ed6997f2
Shiv Raj Badu <shivrajbadu@gmail.com>
2024-02-11 16:47:38 +0545

    categories and files renamed

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1707490865';
+const cacheName = 'chirpy-1707649186';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
