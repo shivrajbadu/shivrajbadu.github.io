9ca8ac7f57fe7aa07c7358e2cc106d286b76a6bc
Shiv Raj Badu <shivrajbadu@gmail.com>
2025-05-29 17:23:46 +0545

    notes on authentication types added

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1748508118';
+const cacheName = 'chirpy-1748518634';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
