b00d0302d21888dbd18df7bf0711510f755c2d06
Shiv Raj Badu <shivrajbadu@gmail.com>
2025-06-04 22:57:49 +0545

    ruby group_by notes added

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1748518634';
+const cacheName = 'chirpy-1749057101';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
