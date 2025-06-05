646fd6056aa7faf21a4ca0a81c539741d28bb25d
Shiv Raj Badu <shivrajbadu@gmail.com>
2025-06-05 14:36:41 +0545

    notes added on exception-handling, regex

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1749112841';
+const cacheName = 'chirpy-1749113454';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
