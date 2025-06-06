78a3ff50188c93fc4dbe26275cdc775628c1f717
Shiv Raj Badu <shivrajbadu@gmail.com>
2025-06-06 20:00:55 +0545

    added powerbi visualization walkthrough notes

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1749113454';
+const cacheName = 'chirpy-1749219107';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
