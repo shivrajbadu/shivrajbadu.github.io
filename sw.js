efb5c41f0648251c007b33c3192cbca1c22ea3f6
Shiv Raj Badu <shivrajbadu@gmail.com>
2025-07-01 20:13:12 +0545

    Blog on The Cost of Perfectionism in Dogfooding

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1749224393';
+const cacheName = 'chirpy-1751379941';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
