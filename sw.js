0e6121e65f517c0a1711157a20b4e29150e17fcd
Shiv Raj Badu <shivrajbadu@gmail.com>
2025-07-16 20:41:53 +0545

    Post error resolution notes added

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1752669913';
+const cacheName = 'chirpy-1752677764';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
