02f52ff5d32edfed29a9d917a27b1891b17df059
Shiv Raj Badu <shivrajbadu@gmail.com>
2025-07-16 17:00:26 +0545

    corrected date

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1752664060';
+const cacheName = 'chirpy-1752664481';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
