6e116c2604af54d7f799b0776e8fc6f0920b5a6c
Shiv Raj Badu <shivrajbadu@gmail.com>
2025-07-17 00:10:11 +0545

    Add nginx.conf and volume mount to proxy Rails container through Terraform-managed nginx. Configure nginx as reverse proxy for Rails app via Terraform.

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1752677764';
+const cacheName = 'chirpy-1752690273';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
