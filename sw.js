bee313494b4f6aab4c65db84a832a0a64e67b480
Shiv Raj Badu <shivrajbadu@gmail.com>
2025-07-27 17:54:07 +0545

    Blog on Qwen-code added

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1752690273';
+const cacheName = 'chirpy-1753618111';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
