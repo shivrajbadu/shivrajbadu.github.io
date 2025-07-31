edc4d96df245764ebcb39553b2973d3a0ae16f1c
Shiv Raj Badu <shivrajbadu@gmail.com>
2025-07-31 18:31:29 +0545

    docs: add Rails 8 modern development guide

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1753723825';
+const cacheName = 'chirpy-1753965664';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
