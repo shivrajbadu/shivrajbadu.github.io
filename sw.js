30ac8e3c3585d478ed4c648d117729287fa81db9
Shiv Raj Badu <shivrajbadu@gmail.com>
2025-08-01 12:38:14 +0545

    fixes: corrected dir name

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1754030075';
+const cacheName = 'chirpy-1754031157';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
