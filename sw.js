3679b190067b251ffe4f07cd9f677e69682cf9b4
Shiv Raj Badu <shivrajbadu@gmail.com>
2025-08-01 12:20:54 +0545

    feat: notes on library ruby_llm updated

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1753985965';
+const cacheName = 'chirpy-1754030075';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
